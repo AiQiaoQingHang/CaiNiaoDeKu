dfc01662978a17f49c999c91f98fe2b8ae44f642
嵌入式好难啊 <3406824532@qq.com>
2025-10-15 00:32:46 +0800

    Delete 四轴飞行器（未经实物检验） directory

--- 四轴飞行器（未经实物检验）/A遥控器程序/user/USART.h
+++
@@ -1,5 +0,0 @@
-#ifndef USART_H
-#define USART_H
-
-void My_USART_Init(uint16_t BaudRate);				//USART1≥ı ºªØ
-#endif
--- 四轴飞行器（未经实物检验）/A遥控器程序/user/watch_dog.h
+++
@@ -1,9 +0,0 @@
-#ifndef WATCHDOG_H
-#define WATCHDOG_H
-
-
-#define Dog_time 96											//看门狗时间，计算公式【1.6ms * x】，范围【0x00-0xFFF】
-void Watch_dog_Init(void);							//看门狗初始化
-
-
-#endif
